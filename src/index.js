10eef30288fcb7b5eacda8795ed763858d3bc1d1
Kishlay Singh <kishlayharsh140@gmail.com>
2025-04-22 16:41:25 +0530

    Deployment commit

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "./style.css"
-import { project } from "./projectDOM"
-
-const aside = document.querySelector("aside")
-project.render(aside)
--- src/storage.js
+++
@@ -1,9 +0,0 @@
-// src/storage.js
-
-import { pubsub } from "./pubsub";
-
-export const saveProjects = (projects) => {
-    localStorage.setItem('projectList', JSON.stringify(projects));
-    const getList = localStorage.getItem('projectList');
-    pubsub.publish('projectaddedToStorage', JSON.parse(getList));
-}
